d9b47642e9e6dd287da617d20cfb736b6b18efa3
pasdam <pasdam@users.noreply.github.com>
2019-07-10 18:53:50 +0800

    Fix checkstyle rules

--- src/main/java/com/pasdam/javatemplate/Application.java
+++ src/main/java/com/pasdam/javatemplate/Application.java
@@ -2,8 +2,19 @@ package com.pasdam.javatemplate;
 
 import org.slf4j.LoggerFactory;
 
-public class Application {
+/**
+ * Example application.
+ */
+public final class Application {
 
+	private Application() {}
+
+	/**
+	 * Main method.
+	 *
+	 * @param args
+	 * 			arguments from command line
+	 */
 	public static void main(String[] args) {
 		LoggerFactory.getLogger(Application.class).info("Hello world");
 	}
